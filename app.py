4d7788d60228c52de3c34ef374bed7e496d27cb5
Damilare Idowu <aideedrey@gmail.com>
2026-04-18 21:08:45 +0100

    Update health endpoint

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
+import psutil
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-
+process = psutil.Process()
 
 @app.route("/")
 def home():
     return jsonify({"message": "API is running"}), 200
 
-
 @app.route("/health")
 def health():
-    return jsonify({"message": "healthy"}), 200
+    cpu_usage = f"{psutil.cpu_percent(interval=0.1):.2f}%"
+    memory_usage = f"{process.memory_info().rss / (1024 * 1024):.2f}MB"
 
+    return jsonify({
+        "message": "healthy",
+        "cpu": cpu_usage,
+        "memory": memory_usage
+    }), 200
 
 @app.route("/me")
 def me():
@@ -20,7 +26,6 @@ def me():
         "email": "aideedrey@gmail.com",
         "github": "https://github.com/Dreybest/hng-stage1"
     }), 200
-
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
