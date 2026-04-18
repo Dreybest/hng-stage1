feb9db82cbb388123aacdf9f04ed0d1c579344e7
Damilare Idowu <aideedrey@gmail.com>
2026-04-19 00:05:35 +0100

    Update all

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import psutil
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-process = psutil.Process()
 
 @app.route("/")
 def home():
@@ -10,21 +8,14 @@ def home():
 
 @app.route("/health")
 def health():
-    cpu_usage = f"{psutil.cpu_percent(interval=0.1):.2f}%"
-    memory_usage = f"{process.memory_info().rss / (1024 * 1024):.2f}MB"
-
-    return jsonify({
-        "message": "healthy",
-        "cpu": cpu_usage,
-        "memory": memory_usage
-    }), 200
+    return jsonify({"message": "healthy"}), 200
 
 @app.route("/me")
 def me():
     return jsonify({
         "name": "Damilare Idowu",
         "email": "aideedrey@gmail.com",
-        "github": "https://github.com/Dreybest/hng-stage1"
+        "github": "https://github.com/Dreybest"
     }), 200
 
 if __name__ == "__main__":
