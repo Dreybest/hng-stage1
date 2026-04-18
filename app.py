274c8440e59ae116d18f731c8fd4ff74d5bfc29f
Damilare Idowu <aideedrey@gmail.com>
2026-04-18 17:37:48 +0100

    Update API

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({"message": "API is running"}), 200
 
+
 @app.route("/health")
 def health():
     return jsonify({"message": "healthy"}), 200
+
 
 @app.route("/me")
 def me():
@@ -17,6 +20,7 @@ def me():
         "email": "aideedrey@gmail.com",
         "github": "https://github.com/Dreybest"
     }), 200
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
